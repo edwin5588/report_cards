dac9d31f6c64ad2384def3f8491fb8be2c511bfa
Edwin Huang <43209173+edwin5588@users.noreply.github.com>
2021-12-29 23:29:48 -0800

    updates to main

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import sys
+import pandas as pd
+import numpy as np
+import json
 
 
+
+def csv_to_pd(fp):
+    """
+    converts CSV to a pandas dataframe object
+
+    """
+    return None
 if __name__ == "__main__":
-    arguments = sys.argv
 
+    arguments = sys.argv
+    dummy, courses_csv, students_csv, tests_csv, marks_csv, output_json = arguments
     for argument in arguments:
         print(argument)
-    print('Number of arguments:', len(sys.argv), 'arguments.')
-    print('Argument List:', str(sys.argv))
